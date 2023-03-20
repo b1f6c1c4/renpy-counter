1c982d8c3221c51de39d57610968b9ceea901cef
b1f6c1c4 <b1f6c1c4@gmail.com>
2023-03-19 21:34:29 -0400

    jest simple

--- index.test.js
+++ index.test.js
@@ -1,5 +1,33 @@
 const RenpyCounter = require('./index.js');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+const count = (verse) => {
+  const counter = new RenpyCounter();
+  verse.split('\n').forEach((line, id) => {
+    counter.parseLine(line, id);
+  });
+  return counter;
+};
+
+describe('parser', () => {
+  describe('indent', () => {
+    const failing = (verse) => () => count(verse);
+    test('init', () => {
+      expect(failing(`
+  a "hello"
+`)).toThrow();
+    });
+    test('not colon', () => {
+      expect(failing(`
+a "hello"
+  b "world"
+`)).toThrow();
+    });
+    test('not full', () => {
+      expect(failing(`
+label hello:
+  b "world"
+ b "world"
+`)).toThrow();
+    });
+  });
 });
